f2da19f7c701749cef69210e32d6178d2c38431c
Lars Vilhuber <lars@vilhuber.com>
2023-09-16 06:33:26 +0000

    Cleaning up license management

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,12 +1,13 @@
  #!/bin/sh
  if [ ! -f stata.lic ]
  then
-    if [ -z ${STATA_LIC_BASE64} ]
+    if [ -z "${STATA_LIC_BASE64}" ]
     then
         echo "No license found."
         exit 2
     else
-        echo "${STATA_LIC_BASE64}" | base64 -d > stata.lic 
+        #echo "${STATA_LIC_BASE64}" | base64 -d > stata.lic 
+        echo "No license created at init, none present"
     fi
 fi
 #docker buildx install
--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,13 +1,15 @@
  #!/bin/sh
  if [ ! -f stata.lic ]
  then
-    if [ -z ${STATA_LIC_BASE64} ]
+    if [ -z "${STATA_LIC_BASE64}" ]
     then
         echo "No license found."
         exit 2
-    else
-        echo "${STATA_LIC_BASE64}" | base64 -d > /home/statauser/stata.lic 
+    else 
+        echo "${STATA_LIC_BASE64}" | base64 -d > /tmp/stata.lic
+        [ -f /tmp/stata.lic ] && sudo mv /tmp/stata.lic /usr/local/stata/stata.lic
+        [ -f /usr/local/stata/stata.lic ] && sudo chmod -w /usr/local/stata/stata.lic 
     fi
 fi
 #docker buildx install
-echo "init done."
+echo "post done."
